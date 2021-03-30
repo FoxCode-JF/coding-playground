0320da4986edfb1ca52c0c83cb9c711f2b321823
Fox <filipiukjarek@gmail.com>
2021-03-30 20:20:39 +0200

    Memalloc with first fir strategy implemented

--- src/memoryAllocator/memalloc.c
+++ src/memoryAllocator/memalloc.c
@@ -1,16 +1,88 @@
-// Simple malloc
+// Simple malloc based on https://arjunsreedharan.org/post/148675821737/write-a-simple-memory-allocator
+
 #include "memalloc.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <vcruntime.h>
 
-void * memalloc(size_t size)
+header_t *head, *tail;
+
+pthread_mutex_t global_malloc_lock;
+
+void *memalloc(size_t size)
 {
+	size_t total_size;
 	void *block;
-	block = sbrk(size);
-	if (block == (void*) -1)
+	header_t *header = NULL;
+
+	if (size == 0)
 	{
 		return NULL;
 	}
 
-	return block;
+	pthread_mutex_lock(&global_malloc_lock);
+	header = get_free_block(size);
+
+	if (header != NULL)
+	{
+		header->s.is_free = false;
+		pthread_mutex_unlock(&global_malloc_lock);
+		return (void*)(header + 1);					// + 1 shifts pointer to the byte right after the header
+	}
+
+	total_size = sizeof(header_t) + size;
+	block = sbrk(total_size);
+
+	if (block == (void*) -   1)
+	{
+		pthread_mutex_unlock(&global_malloc_lock);
+		return NULL;
+	}
+
+	header = block;
+	header->s.size = size;
+	header->s.is_free = false;
+	header->s.next = NULL;
+
+	if (head == NULL)
+	{
+		head = header;
+	}
+	if (tail != NULL)
+	{
+		tail->s.next = header;
+	}
+
+	tail = header;
+	pthread_mutex_unlock(&global_malloc_lock);
+	return (void*)(header + 1);
+}
+
+
+void memfree(void *block)
+{
+	
+}
+
+/**
+ * @brief      Gets the free block using first fit strategy
+ *
+ * @param[in]  size  The size of requested block in bytes
+ *
+ * @return     pointer to the free block of memory
+ */
+static header_t *get_free_block(size_t size)
+{
+	header_t *curr = head;
+	while (curr) 
+	{
+		if (curr->s.is_free && curr->s.size >= size)
+		{
+			return curr;
+		}	
+		curr = curr->s.next;
+	}
+	return NULL;
 }
--- src/memoryAllocator/memalloc.h
+++ src/memoryAllocator/memalloc.h
@@ -1,4 +1,21 @@
 #include <unistd.h>
+#include <pthread.h>
+
+typedef uint8_t ALIGN[16];
+
+union header
+{
+	struct header_t
+	{
+		size_t size;
+		bool is_free;
+		struct header_t *next;
+	} s;
+	ALIGN stub;
+};
+
+typedef union header header_t;
 
 
-size_t DEFAULT_BLOCK_SIZE = 0;
+void *memalloc(size_t size);
+header_t *get_free_block(size_t size);
