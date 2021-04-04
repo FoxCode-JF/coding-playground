f16d17f9716b6ef253af2d55ce2d5f50bff02e59
Fox <filipiukjarek@gmail.com>
2021-04-04 23:35:03 +0200

    Implementation of mofular exponent using openssl big numbers library

--- src/fastModularExponent/fast_modular_exponent.h
+++ src/fastModularExponent/fast_modular_exponent.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stdint.h>
+#include <openssl/bn.h>
+#include <openssl/ossl_typ.h>
 
-int modular_exponent(uint64_t number,  uint64_t exponent, uint64_t modulo);
-// int modular_exponent(int b, int e, int m);
+char *bn_modular_exponent(BIGNUM *base,  BIGNUM *exponent, BIGNUM *modulo);
+int modular_exponent(uint64_t base,  uint64_t exponent, uint64_t modulo);
