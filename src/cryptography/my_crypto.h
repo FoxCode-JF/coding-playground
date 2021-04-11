8579b06388c40864aed95e051be9cf533ae526a6
Fox <filipiukjarek@gmail.com>
2021-04-11 13:59:32 +0200

    random num ber generator BBS implemented

--- src/cryptography/my_crypto.h
+++ src/cryptography/my_crypto.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <stdint.h>
+#include <openssl/bn.h>
+
 char *vernam_cipher_encrypt(const char *message, const char *key);
 char *vernam_cipher_decode(const char *message, const char *key);
 
 char *vigenere_cipher_encrypt(const char *message, const char *key);
 char *vigenere_cipher_decode(const char *message, const char *key);
 
+char *rand_BBS(uint64_t seed, uint64_t p, uint64_t q, int bit_num_len);
 
 
 int my_modulo(int number, int modulo);
